a97143102bad42f116c5e80f71c920db16e0e2f5
jpeng303 <jpeng303>
2026-06-29 22:43:58 -0400

    Update train script with wandb logging

--- scripts/train.py
+++ scripts/train.py
@@ -30,70 +30,83 @@ optimizer = optim.Adam(model.parameters(), lr=1e-3)
 latent_cache = torch.empty((len(dataset), hidden_size))
 latents_filled = False
 
-num_epochs = 2
+num_epochs = 500
 use_temporal_straightening = True
 if use_temporal_straightening:
     straightness_measure = torch.nn.CosineSimilarity()
-#with wandb.init(name="mini-wm") as run:
-for epoch in range(num_epochs):
-    model.train()
-    running_loss = 0.0
 
-    j = 0
-    for data_batch in tqdm.tqdm(dataloader):
-        optimizer.zero_grad()          # clear gradients
-        B = len(data_batch['frame_index'])
-        frame_index = data_batch['frame_index']
-        prior_latents = torch.zeros((B, hidden_size), dtype=torch.float32)
+with wandb.init(name="mini-wm") as run:
+    for epoch in range(num_epochs):
+        model.train()
+        running_loss = 0.0
+        running_dynamics_loss = 0.0
+        running_sigreg_loss = 0.0
+        running_curvature_loss = 0.0
 
-        if use_temporal_straightening:
-            prior_latents_2 = torch.zeros((B, hidden_size), dtype=torch.float32)
+        for data_batch in tqdm.tqdm(dataloader):
+            optimizer.zero_grad()          # clear gradients
+            B = len(data_batch['frame_index'])
+            frame_index = data_batch['frame_index']
+            prior_latents = torch.zeros((B, hidden_size), dtype=torch.float32)
 
-        active_frames = data_batch['index']
-        for i, (f, x) in enumerate(zip(active_frames, frame_index)):
-            if x > 0:
-                prior_latents[i] = latent_cache[f-1]
-            if use_temporal_straightening and x > 1:
-                prior_latents_2[i] = latent_cache[f-2]
+            if use_temporal_straightening:
+                prior_latents_2 = torch.zeros((B, hidden_size), dtype=torch.float32)
 
-        outputs = model(
-            prior_latents.cuda(),
-            data_batch['observation.tokens'].cuda(),   # x
-            data_batch['observation.token_mask'].cuda(),
-            data_batch['observation.token_categories'].cuda(),
-            data_batch['action'].cuda()
-        )        # forward pass
+            active_frames = data_batch['index']
+            for i, (f, x) in enumerate(zip(active_frames, frame_index)):
+                if x > 0:
+                    prior_latents[i] = latent_cache[f-1]
+                if use_temporal_straightening and x > 1:
+                    prior_latents_2[i] = latent_cache[f-2]
 
-        if latents_filled:
-            latent_targets = latent_cache[active_frames].cuda()
-            pred_loss = (outputs - latent_targets).pow(2).mean()
-        else:
-            pred_loss = 0
+            outputs = model(
+                prior_latents.cuda(),
+                data_batch['observation.tokens'].cuda(),   # x
+                data_batch['observation.token_mask'].cuda(),
+                data_batch['observation.token_categories'].cuda(),
+                data_batch['action'].cuda()
+            )        # forward pass
 
-        sigreg_loss = sigreg(outputs)
-        # Copied from jepawm (lambda=0.09)
-        loss = pred_loss + 0.09 * sigreg_loss
-        if latents_filled and use_temporal_straightening:
-            prior_latents = prior_latents.cuda()
-            velocity = outputs - prior_latents
-            prev_velocity = prior_latents - prior_latents_2.cuda()
-            # Negative: We want it to be high (straight)
-            loss -= straightness_measure(velocity, prev_velocity).mean()
+            if latents_filled:
+                latent_targets = latent_cache[active_frames].cuda()
+                pred_loss = (outputs - latent_targets).pow(2).mean()
+            else:
+                pred_loss = 0
 
-        outputs = outputs.detach().cpu()
-        latent_cache[active_frames] = outputs
+            sigreg_loss = sigreg(outputs)
+            # Copied from jepawm (lambda=0.09)
+            loss = pred_loss + 0.09 * sigreg_loss
+            if latents_filled and use_temporal_straightening:
+                prior_latents = prior_latents.cuda()
+                velocity = outputs - prior_latents
+                prev_velocity = prior_latents - prior_latents_2.cuda()
+                # Negative: We want it to be high (straight)
+                straightness_loss = straightness_measure(velocity, prev_velocity).mean()
+                loss -= straightness_loss
+                running_curvature_loss += straightness_loss.item() * B
 
-        loss.backward()                # backprop
-        optimizer.step()               # update weights
+            outputs = outputs.detach().cpu()
+            latent_cache[active_frames] = outputs
 
-        running_loss += loss.item() * B
-        j += 1
-        if j > 10:
-            break
+            loss.backward()                # backprop
+            optimizer.step()               # update weights
 
-    epoch_loss = running_loss / len(dataset)
-    print(f"Epoch {epoch+1}/{num_epochs} — loss: {epoch_loss:.4f}")
-    latents_filled = True
+            running_loss += loss.item() * B
+            running_dynamics_loss += pred_loss.item() * B
+            running_sigreg_loss += sigreg_loss.item() * B
+
+        epoch_loss = running_loss / len(dataset)
+        epoch_dynamics_loss = running_dynamics_loss / len(dataset)
+        epoch_sigreg_loss = running_sigreg_loss / len(dataset)
+        epoch_curvature_loss = running_curvature_loss / len(dataset)
+        run.log({
+            "loss": epoch_loss,
+            "pred_loss": epoch_dynamics_loss,
+            "sigreg_loss": epoch_sigreg_loss,
+            "curvature_loss": epoch_curvature_loss,
+        })
+        print(f"Epoch {epoch+1}/{num_epochs} — loss: {epoch_loss:.4f}")
+        latents_filled = True
 
 torch.save({
     "epoch": epoch,
